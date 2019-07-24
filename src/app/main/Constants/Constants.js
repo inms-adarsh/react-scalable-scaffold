0a506b66104b8a558f0aa95154ac3361a421f331
jainyesha1989 <harshverma04111989@gmail.com>
2019-07-24 18:00:50 +0530

    Merge branch 'skinning-task' of https://github.com/inms-adarsh/react-scalable-scaffold into skinning-task
    # Conflicts:
    #	package.json
    #	src/app/main/apps/input-components/EditButton.js
    #	src/app/main/apps/inputComponents/EditButton.js
    #	src/app/main/apps/movies/Movies/MoviesList.js
    #	src/app/main/components/forms/EditButton.js
    #	src/app/main/components/forms/InputBox.js
    #	src/app/main/components/forms/SelectBoxWithEdit.js
    #	src/app/main/components/forms/TextArea.js
    #	src/app/main/components/forms/inputComponents.scss

---
+++ src/app/main/Constants/Constants.js
@@ -0,0 +1,20 @@
+export const TRUCK_LOAD = [
+    { label: 'FTL', value: 'FTL' },
+    { label: 'LTL', value: 'LTL' }
+  ];
+export const LOAD_BEARER = [
+    { label: 'Customer', value: 'Customer' },
+    { label: 'Carrier', value: 'Carrier' },
+    { label: 'Broker', value: 'Broker' }
+  ];
+export const ENTER_ADD_CHECK = [{ label: 'Enter Address Manually', value: 'manualAddressCheck' }];
+export const INDIA = ['India'];
+export const STATE= [
+    "Andaman and Nicobar Islands",
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam","Bihar","Chandigarh","Chhattisgarh","Dadra and Nagar Haveli","Daman and Diu","Delhi","Goa",
+    "Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala",
+    "Lakshadweep","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha",
+    "Puducherry","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand",
+    "West Bengal"]
