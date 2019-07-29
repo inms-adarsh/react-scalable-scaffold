332d6fb0f30a798a8fca603345fc54815860355f
jainyesha1989 <harshverma04111989@gmail.com>
2019-07-29 18:44:16 +0530

    commiting code to ensure dynamic json using forms

--- src/app/main/apps/dashboard/DashBoard/DashBoardContainer.js
+++ src/app/main/apps/dashboard/DashBoard/DashBoardContainer.js
@@ -4,7 +4,15 @@ import DashBoardView from './DashBoardView';
 class DashBoardContainer extends Component  {
     render () {
         return (
-            <DashBoardView />
+            <DashBoardView
+            modelData={[
+              { name: "name", label: "Name", props: { required: true } },
+              { name: "age", label: "Age", type: "number" }
+            ]}
+            onSubmit={model => {
+              this.onSubmit(model);
+            }}
+          />
         )
     }
 }
--- src/app/main/apps/dashboard/DashBoard/DashBoardView.js
+++ src/app/main/apps/dashboard/DashBoard/DashBoardView.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {Pane, Heading} from 'evergreen-ui';
+import Formsy from 'formsy-react';
 
-const DashBoardView = () => {
+import {Pane, Heading} from 'evergreen-ui';
+import InputBox from '../../../components/forms/InputBox';
+
+const DashBoardView = (props) => {
+    let data = props.modelData;
     return (
         <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
             <Pane flex={1} alignItems="center" display="flex">
                 <Heading size={600}>DashBoard</Heading>
             </Pane>
-        
+            <Formsy onValidSubmit={props.onSubmit}>
+             {
+                data.map(m => 
+                    <InputBox name={m.name} label={m.label} required ={m.props.required} />
+                ) 
+             }
+
+            </Formsy>
         </Pane>
+       
     )
 
 }
